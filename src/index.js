636c85ccf9cde80dfde149360a0b3e4e76521255
RESEGO MOTLHASI <remotlhjhb024@student.wethinkcode.co.za>
2024-10-23 12:48:37 +0200

    Added basic JavaScript functionality to the search input, no API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 function searchBar(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#js-search-bar");
-  let h1 = document.querySelector("#js-city-display");
-  h1.innerHTML = `${searchInput.value}`;
+  let cityDisplay = document.querySelector("#js-city-display");
+  cityDisplay.innerHTML = `${searchInput.value}`;
 }
 
 let form = document.querySelector("form");
