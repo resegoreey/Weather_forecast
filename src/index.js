d8aba479f93f7d05e4566766a0dbcf3ea97d4cdc
RESEGO MOTLHASI <remotlhjhb024@student.wethinkcode.co.za>
2024-10-23 13:36:48 +0200

    Added the search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+function weatherDisplay(response) {
+  let temperature = Math.round(response.data.temperature.current);
+  let city = document.querySelector("#js-city-display");
+  city.innerHTML = response.data.city;
+  let tempDisplay = document.querySelector("#js-temperature");
+  tempDisplay.innerHTML = temperature;
+}
+
 function searchBar(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#js-search-bar");
-  let cityDisplay = document.querySelector("#js-city-display");
-  cityDisplay.innerHTML = `${searchInput.value}`;
+  let city = searchInput.value;
+
+  let apiKey = "ee0d5af457458510ac3bat64c8bo7383";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(weatherDisplay);
 }
 
 let form = document.querySelector("form");
